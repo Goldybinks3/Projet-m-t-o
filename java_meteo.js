ff600a8384f9f43b1606ea71248e8bd7a86fb764
Goldybinks3 <166026964+Goldybinks3@users.noreply.github.com>
2024-04-05 11:16:43 +0200

    Update java_meteo.js

--- java_meteo.js
+++ java_meteo.js
@@ -1,6 +1,13 @@
+const communeSelect = document.getElementById("city");
+
 function Recup_Villes(){
     let saisie = document.getElementById("postalCode").value;
     return saisie
     }
+
+data.forEach((commune) => {
+        const option = document.createElement("comms");
+        comms.value = commune.code;
+        comms.textContent = commune.nom;
 
 fetch('https://geo.api.gouv.fr/communes?codePostal=${saisie}')
