0b8cd264876816577dc1374a610fc7064236a2d0
Maelrnd <166027243+Maelrnd@users.noreply.github.com>
2024-04-15 17:14:12 +0200

    Update java_meteo.js

--- java_meteo.js
+++ java_meteo.js
@@ -32,26 +32,26 @@ document.addEventListener("DOMContentLoaded", async function() { // Attend que l
 
 //const communeSelect = document.getElementById("city");
 
-function Recup_Villes(){
-    let saisie = document.getElementById("postalCode").value;
-    return saisie
-}
+//function Recup_Villes(){
+  // let saisie = document.getElementById("postalCode").value;
+//return saisie
+//}
 
-function fetchCommunesByCodePostal(codePostal) {
-    const response = await fetch(
-        `https://geo.api.gouv.fr/communes?codePostal=${codePostal}`
-    );
-    const data = await response.json();
-    return data;
-}
+//function fetchCommunesByCodePostal(codePostal) {
+    //const response = await fetch(
+      //  `https://geo.api.gouv.fr/communes?codePostal=${codePostal}`
+    //);
+    //const data = await response.json();
+  //  return data;
+//}
 
-function displayCommunes(data) {
-    data.forEach((commune) => {
-            const option = document.createElement("option");
-            option.value = commune.code;
-            option.textContent = commune.nom;
-    });
-}
+//function displayCommunes(data) {
+  //  data.forEach((commune) => {
+    //        const option = document.createElement("option");
+      //      option.value = commune.code;
+        //    option.textContent = commune.nom;
+    //});
+//}
 
 //const data = await fetchCommunesByCodePostal(codePostal);
 //displayCommunes(data);
