4375a95060d2e0530b60cea14f068de7a190467a
Maelrnd <166027243+Maelrnd@users.noreply.github.com>
2024-04-15 16:58:40 +0200

    Update java_meteo.js

--- java_meteo.js
+++ java_meteo.js
@@ -27,3 +27,35 @@ document.addEventListener("DOMContentLoaded", async function() { // Attend que l
     }
 });
 
+
+//code de noa 
+
+const communeSelect = document.getElementById("city");
+
+function Recup_Villes(){
+    let saisie = document.getElementById("postalCode").value;
+    return saisie
+}
+
+function fetchCommunesByCodePostal(codePostal) {
+    const response = await fetch(
+        `https://geo.api.gouv.fr/communes?codePostal=${codePostal}`
+    );
+    const data = await response.json();
+    return data;
+}
+
+function displayCommunes(data) {
+    data.forEach((commune) => {
+            const option = document.createElement("option");
+            option.value = commune.code;
+            option.textContent = commune.nom;
+    });
+}
+
+const data = await fetchCommunesByCodePostal(codePostal);
+displayCommunes(data);
+
+fetch('https://geo.api.gouv.fr/communes?codePostal=${saisie}')
+
+
